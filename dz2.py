4b4474264c8443244f78538763ec6210c36a3a9b
Frofe <98185433+Aleshka274564@users.noreply.github.com>
2026-09-23 19:57:37 +0300

    Update dz2.py

---
+++ dz2.py
@@ -0,0 +1,21 @@
+import random
+
+def get_numbers_ticket(min, max, quantity):
+
+    if min < 1 or max > 1000 or quantity < min or quantity > max:
+        return []
+    
+    # случайные числа
+    numbers = random.sample(range(min, max + 1), quantity)
+    
+    # cортируем список 
+    numbers.sort()
+    
+    # результат
+    return numbers
+
+lottery_numbers = get_numbers_ticket(1, 67, 6)
+print("Ваші лотерейні числа:", lottery_numbers)
+
+bad_ticket = get_numbers_ticket(1, 1005, 5)
+print("Перевірка помилки (має бути []):", bad_ticket)
