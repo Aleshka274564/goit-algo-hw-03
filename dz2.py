cf827ce5e4e3dde38ab86233b1a8533796b069f2
Frofe <98185433+Aleshka274564@users.noreply.github.com>
2026-09-23 21:36:42 +0300

    Update dz2.py

--- dz2.py
+++ dz2.py
@@ -2,20 +2,13 @@ import random
 
 def get_numbers_ticket(min, max, quantity):
 
-    if min < 1 or max > 1000 or quantity < min or quantity > max:
-        return []
+    if min < 1 or max > 1000 or quantity < 1 or quantity > (max - min + 1):
     
-    # случайные числa
+        return ["error😢"]
+    
     numbers = random.sample(range(min, max + 1), quantity)
     
-    # cортируем список 
     numbers.sort()
-    
-    # результат
     return numbers
-
-lottery_numbers = get_numbers_ticket(1, 67, 6)
+lottery_numbers = get_numbers_ticket(1, 49, 6)
 print("Ваші лотерейні числа:", lottery_numbers)
-
-bad_ticket = get_numbers_ticket(1, 1005, 5)
-print("Перевірка помилки (має бути []):", bad_ticket)
